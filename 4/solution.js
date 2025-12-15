28dedcf3811ea9406ec1d4e83ca39f892068891c
transgerda <info@martijnvanboven.nl>
2025-12-15 14:28:43 +0100

    dag 4 deel 1

---
+++ 4/solution.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const stream = fs.createReadStream('./input.txt');
+
+stream.on('data', function (chunk) {
+    const rows = chunk.toString().split('\n');
+    let input = new Map();
+    for (const row of rows)
+        input.set(input.size, row.split(''));
+
+    let answer = 0;
+
+    function getValidIndexes(x, y) {
+        return [
+            [x-1, y-1],
+            [x, y-1],
+            [x+1, y-1],
+            [x-1, y],
+            [x+1, y],
+            [x-1, y+1],
+            [x, y+1],
+            [x+1, y+1],
+        ];
+    }
+
+    input.forEach((row, x) => {
+        row.forEach((item, y) => {
+            if (input.get(x)[y] != '@')
+                return;
+
+            const indexes = getValidIndexes(x, y);
+            let rolls = 0;
+
+            indexes.forEach(index => {
+                const [row, col] = index;
+
+                try {
+                    if (input.get(row)[col] == '@')
+                        rolls++;
+                } catch {}
+            })
+
+            if (rolls < 4)
+                answer++;
+        })
+    });
+
+    console.log('Answer:', answer);
+});
